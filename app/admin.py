4317591a3bda86d0fae7f1324786f666e208ac16
Tomas Espinosa <espinosa.tomas@alu.frlp.utn.edu.ar>
2024-06-07 01:24:03 -0300

    ADD(Agrego City para vista admin)

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Breed, Client, Pet, Vet
+from .models import Breed, City, Client, Pet, Vet
 
 # Register your models here.
 admin.site.register(Breed)
+admin.site.register(City)
+admin.site.register(Client)
 admin.site.register(Pet)
 admin.site.register(Vet)
-admin.site.register(Client)
+
